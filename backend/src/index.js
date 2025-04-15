5f489e6aec96ad64308226a5672e0be74b413c5b
TranThangBin <thangdev04@gmail.com>
2025-04-15 11:38:19 +0700

    chore: more environment variable for deployment environment

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,22 +4,34 @@ import cors from "cors";
 import { connect } from "mongoose";
 import { join } from "path";
 import { gameRouter, handleError, handleLog } from "./controller.js";
-import { getDirname, MONGOURI, PORT } from "./utils.js";
+import {
+	getDirname,
+	MONGO_APPNAME,
+	MONGO_DATABASE,
+	MONGO_URI,
+	PORT,
+	MONGO_USER,
+	MONGO_PASS,
+	ALLOWED_ORIGINS,
+} from "./utils.js";
 
 const server = express();
 
 server.use(express.static(join(getDirname(), "..", "public")));
 server.use(express.json());
 server.use(fileUpload({ createParentPath: true }));
-server.use(
-	cors({ origin: ["http://127.0.0.1:3000", "http://localhost:3000"] }),
-);
+server.use(cors({ origin: ALLOWED_ORIGINS }));
 server.use(handleLog);
 server.use(handleError);
 server.use("/game", gameRouter);
 
 try {
-	await connect(MONGOURI);
+	await connect(MONGO_URI, {
+		user: MONGO_USER,
+		pass: MONGO_PASS,
+		appName: MONGO_APPNAME,
+		dbName: MONGO_DATABASE,
+	});
 	console.log("Successfully connected to mongodb");
 	server.listen(PORT, () => {
 		console.log(`Listening at 127.0.0.1:${PORT}`);
--- backend/src/utils.js
+++ backend/src/utils.js
@@ -1,9 +1,16 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-export const MONGOURI =
-	process.env.MONGOURI || "mongodb://mongo:27017/game-store";
+export const MONGO_URI = process.env.MONGO_URI || "mongodb://mongo:27017";
+export const MONGO_DATABASE = process.env.MONGO_DATABASE || "game-store";
+export const MONGO_USER = process.env.MONGO_USER || "example";
+export const MONGO_PASS = process.env.MONGO_PASS || "examplepass";
+export const MONGO_APPNAME = process.env.MONGO_APPNAME || "Cluster0";
 export const PORT = process.env.PORT || 8080;
+export const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS?.split(",") || [
+	"http://127.0.0.1:3000",
+	"http://localhost:3000",
+];
 export function getDirname() {
 	return dirname(fileURLToPath(import.meta.url));
 }
